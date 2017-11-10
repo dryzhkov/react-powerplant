02ed94d704424dee6dd734a08eaf58ff3cfcf933
Dmitry Ryzhkov <dmitry@microsoft.com>
2017-11-09 18:11:06 -0800

    updating webpack for prod release

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const PROD = JSON.parse(!process.env.NODE_ENV || '0');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
+  devtool: PROD ? '' : 'eval',
+  entry: PROD ? './src/index' : [
     'webpack-dev-server/client?http://localhost:3000',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: PROD ? 'bundle.min.js' : 'bundle.js',
     publicPath: '/static/'
   },
   resolve: {
@@ -21,5 +22,13 @@ module.exports = {
       loaders: ['awesome-typescript-loader'],
       include: path.join(__dirname, 'src')
     }]
-  }
+  },
+  plugins: PROD ? [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : []
 };
